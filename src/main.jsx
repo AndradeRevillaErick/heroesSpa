edad6d7a2764638d52b5c4a3e0052a4ac7be9fdb
Erick Andrade <erick_andradhe@hotmail.com>
2023-04-17 11:36:07 -0600

    feat(app): add navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";;
-
 
 import './styles.css';
 import { HeroesApp } from './HeroesApp';
-import { MarvelPage } from './heroes/pages/MarvelPage';
-import { DcPage } from './heroes/pages/DcPage';
-import { LoginPage } from './auth/pages/LoginPage';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HeroesApp />,
-  },
-  {
-    path: "MarvelPage",
-    element: <MarvelPage/>,
-  },
-  {
-    path: "DcPage",
-    element: <DcPage/>,
-  },
-  {
-    path: "login",
-    element: <LoginPage />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HeroesApp/>
   </React.StrictMode>,
 )
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,30 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { LoginPage } from "../auth/pages/LoginPage";
+import { DcPage } from "../heroes/pages/DcPage";
+import { MarvelPage } from "../heroes/pages/MarvelPage";
 
 
 export const AppRouter = () => {
-    return(
-        <>
-        </>
-    );
+
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <MarvelPage />,
+        },
+        {
+            path: "MarvelPage",
+            element: <MarvelPage />,
+        },
+        {
+            path: "DcPage",
+            element: <DcPage />,
+        },
+        {
+            path: "login",
+            element: <LoginPage />,
+        },
+    ]);
+      
+    return <RouterProvider router={router} />;
+
 }
